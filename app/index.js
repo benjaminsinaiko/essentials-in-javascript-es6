966c904de3ceda136cbeba0866b3c4b0b547b438
benjaminsinaiko <ben.jamin@outlook.com>
2018-06-19 00:13:11 -0500

    Section 4, spread operator

--- app/index.js
+++ app/index.js
@@ -1,16 +1,37 @@
-let a = 'hello'
+// SECTION 3
 
-console.log(a)
+// let a = 'hello'
 
-{
-  let a = 'goodbye'
-  console.log('a inside scope', a)
+// console.log(a)
+
+// {
+//   let a = 'goodbye'
+//   console.log('a inside scope', a)
+// }
+
+// const array = [1, 2, 3]
+// array.push(4)
+// console.log(array)
+
+// let b = 'world'
+// let d = `hello ${b}`
+// console.log(d)
+
+// SECTION 4
+
+let a = [7, 8, 9]
+let b = [6, ...a, 10]
+console.log(b)
+
+function print(...z) {
+  console.log(z)
+}
+print(1, 2, 3, 4)
+
+function butter(...z) {
+  let a = [1, 2, 3, ...z]
+  console.log(a)
+  return a
 }
 
-const array = [1, 2, 3]
-array.push(4)
-console.log(array)
-
-let b = 'world'
-let d = `hello ${b}`
-console.log(d)
+butter(4, 5, 6)
