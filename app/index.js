cb5ddb4f58e9c86c8b43691b7c698f4714e79728
benjaminsinaiko <ben.jamin@outlook.com>
2018-06-19 00:01:07 -0500

    Section 3, new es6 syntax

--- app/index.js
+++ app/index.js
@@ -1,2 +1,16 @@
-console.log('hello webpack!')
-console.log('hello in the browser')
+let a = 'hello'
+
+console.log(a)
+
+{
+  let a = 'goodbye'
+  console.log('a inside scope', a)
+}
+
+const array = [1, 2, 3]
+array.push(4)
+console.log(array)
+
+let b = 'world'
+let d = `hello ${b}`
+console.log(d)
