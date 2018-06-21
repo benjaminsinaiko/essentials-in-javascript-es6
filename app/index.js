1c98ede61c4dc0bff9eb8184155058a38c8780c6
benjaminsinaiko <ben.jamin@outlook.com>
2018-06-20 23:16:36 -0500

    Section 6 exporting modules and by default

--- app/index.js
+++ app/index.js
@@ -1,8 +1,18 @@
-import { students } from './students'
-import { total } from './students'
-
 // SECTION 6
 // Exporting values
 
-console.log(students)
-console.log(total)
+// import { students } from './students'
+// import { total } from './students'
+
+// console.log(students)
+// console.log(total)
+
+// SECTION 6
+// Exporting expressions
+
+// import { add, multiply } from './calculator'
+// console.log(add(3, 5))
+// console.log(multiply(3, 5))
+
+import multiply from './calculator'
+console.log(multiply(3, 5))
