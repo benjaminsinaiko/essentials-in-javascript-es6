bb3556edc4ecde891f59202a195b0c4c1059fb80
benjaminsinaiko <ben.jamin@outlook.com>
2018-06-20 23:26:47 -0500

    Section 7, OOP and setting up classes

--- app/index.js
+++ app/index.js
@@ -14,5 +14,22 @@
 // console.log(add(3, 5))
 // console.log(multiply(3, 5))
 
-import multiply from './calculator'
-console.log(multiply(3, 5))
+// import multiply from './calculator'
+// console.log(multiply(3, 5))
+
+// SECTION 7
+// OOP classes
+
+class Entity {
+  constructor(name, height) {
+    this.name = name
+    this.height = height
+  }
+
+  greet() {
+    console.log(`Hi! I'm ${this.name} from middle earth!`)
+  }
+}
+
+let Merry = new Entity('Merry', 4.6)
+Merry.greet()
